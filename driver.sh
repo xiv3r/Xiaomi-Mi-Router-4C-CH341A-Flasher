8dad120eb454310e3c934d10bcb836e1ef091fa2
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-14 18:23:16 +0800

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -2,9 +2,9 @@
 
 sudo apt install bc build-essential gcc cmake make linux-headers-$(uname -r) -y
 mkdir -p ch341a-driver && cd ch341a-driver
-curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/Makefile > Makefile
-curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341.c > ch341.c
-curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341.h > ch341.h
+curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341a-ser/Makefile > Makefile
+curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341a-ser/ch341.c > ch341.c
+curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341a-ser/ch341.h > ch341.h
 sudo make
 sudo make load
 sudo make unload
