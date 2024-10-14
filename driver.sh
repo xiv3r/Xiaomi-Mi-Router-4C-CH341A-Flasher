4b72348b28d9d17c831d2cac3bf1eb2a65524211
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-14 15:49:36 +0800

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 mkdir -p ch341a
+cd ch341
 curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/Makefile > Makefile
+curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341.c > ch341.c
+curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341.h > ch341.h
