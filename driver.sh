70900e6a1f5b0d470889226d09d2015d1ff229da
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-14 18:32:36 +0800

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -5,6 +5,17 @@ mkdir -p ch341a-ser && cd ch341a-ser
 curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341a-ser/Makefile > Makefile
 curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341a-ser/ch341.c > ch341.c
 curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341a-ser/ch341.h > ch341.h
+echo "installing ch341a serial driver"
+sudo make
+sudo make load
+sudo make unload
+sudo make install
+
+cd
+mkdir -p ch341a-par && cd ch341a-par
+curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341a-par/Makefile > Makefile
+curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341a-par/ch34x_pis.c > ch34x_pis.c
+echo "installing ch341a parallel driver"
 sudo make
 sudo make load
 sudo make unload
