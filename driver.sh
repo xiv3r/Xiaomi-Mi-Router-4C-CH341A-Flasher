84980f9d6e70eb49a4777660e97a2af1ed773edc
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-16 19:53:30 +0800

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt install bc build-essential gcc cmake make linux-headers-$(uname -r) -y
+sudo apt install bc build-essential gcc cmake make linux-headers-$(uname -r) cmake g++ libusb-1.0-0-dev qtbase5-dev qttools5-dev pkgconf systemd-dev udev zenity wget -y
 mkdir -p ch341a-ser && cd ch341a-ser
 curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341a-ser/Makefile > Makefile
 curl https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/driver/ch341a-ser/ch341.c > ch341.c
