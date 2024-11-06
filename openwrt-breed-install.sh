adaf0dabeb34f7f0f3fbae71f4a450d6649f7953
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-06 17:45:51 +0800

    Update and rename Xiaomi-R4C-USB-MOD.jpg to openwrt-breed-install.sh

---
+++ openwrt-breed-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+###
+opkg update && opkg install kmod-mtd-rw
+###
+wget -O /tmp/breed.bin https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/Xiaomi_R4C_Breed.bin
+###
+mtd -r write /tmp/breed.bin
