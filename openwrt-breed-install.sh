91953fdb49a1c897e3f15e56df4f97a65e6f2155
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-06-16 21:00:31 +0800

    Update openwrt-breed-install.sh

--- openwrt-breed-install.sh
+++ openwrt-breed-install.sh
@@ -3,6 +3,6 @@
 ###
 opkg update && opkg install kmod-mtd-rw
 ###
-wget -O /tmp/breed.bin https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/Xiaomi_R4C_Breed.bin
+wget -O /tmp/breed.bin https://raw.githubusercontent.com/xiv3r/Xiaomi-Mi-Router-4C-CH341A-Flasher/refs/heads/main/breed-ch.bin
 ###
 mtd -r write /tmp/breed.bin bootloader
